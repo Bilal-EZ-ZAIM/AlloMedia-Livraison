8da21ff346fb38f300d0d0bdaa69c9ef6eebbacd
Bilal-EZ-ZAIM <bilalzail499@gmail.com>
2024-09-25 15:51:14 +0100

    AL-5 j'ai terminé   Envoi d'un e-mail de vérification un compt et terminé l'inscription  avec Nodemailer

---
+++ util/mail.js
@@ -0,0 +1,42 @@
+const nodemailer = require("nodemailer");
+const fs = require("fs");
+const path = require("path");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "bbilalzaimrajawi@gmail.com",
+    pass: "kesupihtnpmhexxq",
+  },
+});
+
+const envoyerEmail = async (email, subject, confirmationLink = null) => {
+  try {
+
+    const htmlTemplate = fs.readFileSync(
+      path.join(__dirname, "public", "confirmationEmail.html"),
+      "utf8"
+    );
+
+    let message = htmlTemplate;
+
+    console.log(message);
+    
+    if (confirmationLink) {
+      message = htmlTemplate.replace("{{confirmationLink}}", confirmationLink);
+    }
+
+    const info = await transporter.sendMail({
+      from: "bbilalzaimrajawi@gmail.com",
+      to: email,
+      subject: subject,
+      html: message,
+    });
+
+    console.log("Message sent: %s", info.messageId);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
+
+module.exports = envoyerEmail;
