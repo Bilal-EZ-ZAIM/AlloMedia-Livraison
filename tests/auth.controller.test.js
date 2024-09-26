2c828a7090c568b376fdb32ae85a6402a2a4d6e7
Bilal-EZ-ZAIM <bilalzail499@gmail.com>
2024-09-26 16:54:25 +0100

    fix error v2 regester tisteng

--- tests/auth.controller.test.js
+++ tests/auth.controller.test.js
@@ -1,14 +1,13 @@
 const request = require("supertest");
 const express = require("express");
 const app = express();
-const { regester } = require("../controller/auth/auth.controller"); // استيراد الدالة
-const User = require("../model/user.model"); // استيراد نموذج المستخدم
+const { regester } = require("../controller/auth/auth.controller"); 
+const User = require("../model/user.model");
 const HashPassword = require("../util/HashPassword");
 const CreateToken = require("../util/createToken");
-const envoyerEmail = require("../util/mail"); // محاكاة إرسال البريد الإلكتروني
-const slug = require("slug"); // استيراد مكتبة slug
+const envoyerEmail = require("../util/mail"); 
+const slug = require("slug"); 
 
-// محاكاة النماذج والمكتبات
 jest.mock("../model/user.model");
 jest.mock("../util/HashPassword");
 jest.mock("../util/createToken");
@@ -16,11 +15,11 @@ jest.mock("../util/mail");
 jest.mock("slug");
 
 app.use(express.json());
-app.post("/api/auth/register", regester); // إعداد نقطة النهاية
+app.post("/api/auth/register", regester);
 
 describe("POST /api/auth/register", () => {
   beforeEach(() => {
-    jest.clearAllMocks(); // إعادة تعيين المحاكيات قبل كل اختبار
+    jest.clearAllMocks(); 
   });
 
   it("should process req.body and create a new user with a token", async () => {
@@ -45,7 +44,7 @@ describe("POST /api/auth/register", () => {
 
     CreateToken.mockReturnValue("mocked_token");
 
-    envoyerEmail.mockResolvedValue(true); // محاكاة إرسال البريد الإلكتروني
+    envoyerEmail.mockResolvedValue(true); 
 
     const response = await request(app)
       .post("/api/auth/register")
@@ -53,12 +52,10 @@ describe("POST /api/auth/register", () => {
       .expect("Content-Type", /json/)
       .expect(201);
 
-    // التحقق من النتيجة
     expect(response.body.message).toBe("User created successfully!");
     expect(response.body.user).toBeDefined();
     expect(response.body.token).toBeDefined();
 
-    // تحقق من استدعاء الدوال
     expect(slug).toHaveBeenCalledWith("John Doe");
     expect(HashPassword).toHaveBeenCalledWith(userData.password);
     expect(User.create).toHaveBeenCalledWith({
@@ -87,7 +84,6 @@ describe("POST /api/auth/register", () => {
       phone: "123456789",
     };
 
-    // محاكاة فشل إنشاء المستخدم
     User.create.mockRejectedValue(new Error("Database error"));
 
     const response = await request(app)
@@ -95,7 +91,6 @@ describe("POST /api/auth/register", () => {
       .send(userData)
       .expect(400);
 
-    // التحقق من الرسالة
     expect(response.body.message).toBe("An error occurred during registration");
     expect(response.body.error).toBe("Database error");
   });
