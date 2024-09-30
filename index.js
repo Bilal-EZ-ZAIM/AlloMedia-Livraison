ac17f82b983980e30c7115363f83ef858ecb1d8f
Bilal-EZ-ZAIM <bilalzail499@gmail.com>
2024-09-30 09:37:50 +0100

    j'ai terminé les test unite

--- index.js
+++ index.js
@@ -3,22 +3,29 @@ const dotenv = require("dotenv");
 const dbConection = require("./config/database");
 const app = express();
 const authRouter = require("./router/auth/auth.router");
+const cors = require("cors");
 dbConection();
 dotenv.config();
+
+const corsOptions = {
+  origin: "http://example.com",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
 app.use("/api/auth/", authRouter);
 
 app.use((err, req, res, next) => {
-  return res.status(400).json({err});
+  return res.status(400).json({ err });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
- 
 
-module.exports = app; 
-
+module.exports = app;
